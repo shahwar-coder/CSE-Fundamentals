2c84ecb5514a0b543897d0e0e4e661bbaefd7dda
shahwar-coder <159693671+shahwar-coder@users.noreply.github.com>
2025-11-21 22:49:43 +0530

    Create 01_OSI_Model_Basics.py

---
+++ Computer_Networks/01_OSI_Model_Basics.py
@@ -0,0 +1,23 @@
+'''
+Q1. What is the OSI Model?
+Ans:
+The OSI Model is a 7-layer framework that explains how data moves across a network.
+Each layer has a specific job, from physical signals to user-level apps.
+
+• Helps understand networking by breaking it into clear layers.
+'''
+# Memory Trick:
+# All People Seem To Need Data Processing (7→1)
+
+
+
+'''
+Q2. What does the Application Layer (Layer 7) do?
+Ans:
+It provides services to end users through applications like browsers, email, WhatsApp.
+It interacts directly with users.
+
+• Think: “Where users touch the network.”
+'''
+# Examples:
+# HTTP, HTTPS, SMTP, FTP
